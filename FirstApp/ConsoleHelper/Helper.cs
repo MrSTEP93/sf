b63883af89a16cbbc3891e65220c1774448b0e0d
Алексей Шаталов <mrstep93@mail.ru>
2023-10-30 23:30:30 +0300

    Добавьте файлы проекта.

---
+++ FirstApp/ConsoleHelper/Helper.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleHelper
+{
+    public class Helper
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void Help()
+        {
+            Console.WriteLine("I helping you");
+        }
+        public static void WriteLn (string s)
+        {
+            Console.WriteLine(s);
+        }
+        public static void WriteLn(string s, bool prompt)
+        {
+            Console.WriteLine(s);
+            if (prompt)
+            {
+                Console.Write(">> ");
+            }
+        }
+        //public static void ReadLn ()
+        //{
+        //    Console.ReadLine();
+        //}
+        public static string? ReadLn ()
+        {
+            string s = Console.ReadLine();
+            return s;
+        }
+    }
+}
---
+++ FirstApp/FirstApp.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using static ConsoleHelper.Helper;
+
+namespace FirstApp.ConsoleApp
+{
+    internal class Program
+    {
+        public static bool YesOrNo(string question, string positiveReply, string negativeReply)
+        {
+            WriteLn(question,true);
+            string answerYN = ReadLn().ToLower();
+            if ((answerYN == "да") & (answerYN == "yes"))
+            {
+                WriteLn(positiveReply);
+                return true;
+            }
+            else
+            {
+                WriteLn(negativeReply);
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            WriteLn("Здравствуйте, студент. Как Вас зовут? (введите ответ после двух угловых скобок)", true);
+            string name = ReadLn();
+            WriteLn($"Здравствуйте, {name}");
+            bool answer = YesOrNo($"Вы уже знакомы с IT, {name}?", "Вау!", "Жаль... Было бы здорово разбираться в общих понятиях" +
+                ", чтобы учиться на разработчика. Но надеюсь, что вы сможете достигнуть результатов и так!!! Удачи!");
+            if (answer)
+            {
+                answer = YesOrNo("А с языком C# ??", $"{name}, вы выглядите достаточно умным!", "Именно для изучения его мы здесь и собрались!");
+                if (answer)
+                {
+                    WriteLn($"Так зачем вы учитесь, {name}?", true);
+                    ReadLn();
+                    WriteLn("Удачи в достижениии поставленных целей!!!");
+                }
+            }
+            WriteLn("До свидания!");
+        }
+    }
+}
