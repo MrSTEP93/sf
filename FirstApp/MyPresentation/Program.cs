8ab59375e051e64f3896e5a62642586c51491b9f
Shatalov.AG <mrstep93@ya.ru>
2023-11-13 23:06:31 +0300

    добавлена презентация (задание на типы данных)

---
+++ FirstApp/MyPresentation/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MyPresentation
+{
+    using System;
+
+    class MyPresentation
+    {
+        public static void Main(string[] args)
+        {
+
+            string myName = "Aleksey";
+            byte myAge = 30;
+            bool haveAPet = true;
+            double myShoeSize = 42;
+
+            Console.WriteLine("My name is " + myName);
+            Console.WriteLine("My age is " + myAge);
+            Console.WriteLine("Do I have a pet? " + haveAPet);
+            Console.WriteLine("My shoe size is " + myShoeSize);
+
+        }
+    }
+}
