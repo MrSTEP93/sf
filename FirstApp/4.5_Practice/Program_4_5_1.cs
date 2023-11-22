d0f171f3d8dea000313875719855ed8acb47db82
Shatalov.AG <mrstep93@ya.ru>
2023-11-23 00:04:58 +0300

    added colsole helper for net 5.0

---
+++ FirstApp/4.5_Practice/Program_4_5_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static ConsoleHelper_50.Helper_50;
+
+namespace m4_5_Practice
+{
+    internal class Program_4_5_1
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string LastName, string Login, byte LoginLength, bool HasPet, string[] favcolors, double Age) User;
+            WriteLn("Type your name:", true);
+            User.Name = ReadLn();
+            WriteLn("Type your last name:", true);
+            User.LastName = ReadLn();
+            WriteLn("Type your login:", true);
+            User.Login = ReadLn();
+            User.LoginLength = (byte)User.Login.Length;
+
+            TryFillDouble("How old are you? (type a number)", out User.Age);
+
+            TryFillBool("Do you have a pet? (type \"true\" or \"false\")", out User.HasPet);
+
+            TryFillByte("How much favourite colors do you have?", out byte colorCount);
+            User.favcolors = new string[colorCount];
+            for (int i = 0; i < colorCount; i++)
+            { 
+                WriteLn("Type your favourite color:", true);
+                User.favcolors[i] = ReadLn();
+            }
+
+            WriteLn("Form completed successfuly, congratulations! Thank you!", ConsoleColor.DarkGreen);
+        }
+    }
+}
---
+++ FirstApp/ConsoleHelper_50/Helper_50.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ConsoleHelper_50
+{
+    public class Helper_50
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void Help()
+        {
+            Console.WriteLine("I helping you");
+        }
+        public static void WriteLn(string s)
+        {
+            Console.WriteLine(s);
+        }
+        public static void WriteLn(string s, bool prompt)
+        {
+            WriteLn(s);
+            if (prompt)
+            {
+                Console.Write(">> ");
+            }
+        }
+        public static void WriteLn(string s, ConsoleColor textColor)
+        {
+            Console.ForegroundColor = textColor;
+            WriteLn(s);
+            Console.ResetColor();
+        }
+
+        public static string? ReadLn()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            string s = Console.ReadLine();
+            Console.ResetColor();
+            return s;
+        }
+        public static void TryFillBool(string s, out bool value)
+        {
+            bool result;
+            do
+            {  
+                WriteLn(s, true);
+                result = bool.TryParse(ReadLn(), out value);
+                if (!result)
+                    WriteLn("Incorrect value!", ConsoleColor.Red);
+            } while (!result);
+        }
+        public static void TryFillByte(string s, out byte value)
+        {
+            bool result;
+            do
+            {
+                WriteLn(s, true);
+                result = byte.TryParse(ReadLn(), out value);
+                if (!result)
+                    WriteLn("Incorrect value!", ConsoleColor.Red);
+            } while (!result);
+        }
+        public static void TryFillDouble(string s, out double value)
+        {
+            bool result;
+            do
+            {
+                WriteLn(s, true);
+                result = double.TryParse(ReadLn(), out value);
+                if (!result)
+                    WriteLn("Incorrect value!", ConsoleColor.Red);
+            } while (!result);
+        }
+        public static void TryFillInt(string s, out int value)
+        {
+            bool result;
+            do
+            {
+                WriteLn(s, true);
+                result = Int32.TryParse(ReadLn(), out value);
+                if (!result)
+                    WriteLn("Incorrect value!", ConsoleColor.Red);
+            } while (!result);
+        }
+    }
+}
